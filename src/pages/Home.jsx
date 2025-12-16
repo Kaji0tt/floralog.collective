2e64cd9f0b65e2117006979f7502d8997c518d12
Jascha Kruse <kaji0tt@googlemail.com>
2025-12-16 18:55:52 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -319,7 +319,7 @@ export default function Home() {
   const getWeekNumber = (date = new Date()) => {
     const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
     const dayNum = d.getUTCDay() || 7;
-    d.setUCDate(d.getUTCDate() + 4 - dayNum);
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
     const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
     const weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
     return `${d.getUTCFullYear()}-W${String(weekNo).padStart(2, '0')}`;
