877abb5d1512c11f794606d3dd8d3e9f04aded53
Jascha Kruse <kaji0tt@googlemail.com>
2025-12-22 13:31:18 +0000

    Manual code change

--- src/pages/QuestNotificationTemplate.jsx
+++ src/pages/QuestNotificationTemplate.jsx
@@ -7,7 +7,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-const DEFAULT_IMAGE = "https://blauzahn.eu/PlantDexIcon.png";
+const DEFAULT_IMAGE = "https://blauzahn.eu/PlantDex/PlantExplorer.png";
 
 export default function QuestNotificationTemplate() {
   const [showNotification, setShowNotification] = useState(false);
