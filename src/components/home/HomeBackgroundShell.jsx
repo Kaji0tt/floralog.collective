8a1c3913a5f81d9e5ff0557db4c14193d2a005ae
Kaji0tt <Hydro-Jascha@web.de>
2026-04-13 21:48:06 +0200

    Update HomeBackgroundShell.jsx

--- src/components/home/HomeBackgroundShell.jsx
+++ src/components/home/HomeBackgroundShell.jsx
@@ -15,7 +15,7 @@ export default function HomeBackgroundShell({ user, getRgbaFromRgb, children }) 
         } : {
           background: isLightUi
             ? "radial-gradient(circle at top, rgb(255, 248, 220) 0%, rgb(244, 231, 187) 52%, rgb(236, 217, 156) 100%)"
-            : "radial-gradient(circle at top, rgb(242, 243, 167) 0%, rgb(87, 117, 98) 60%, rgb(10, 30, 18) 100%)",
+            : "radial-gradient(circle at top, rgb(223, 224, 127) 0%, rgb(67, 146, 96) 60%, rgb(53, 90, 68) 100%)",
         }}
       />
       <div className={`absolute inset-0 ${isLightUi ? "backdrop-blur-[2px] bg-white/20" : "backdrop-blur-3xl"}`} />
